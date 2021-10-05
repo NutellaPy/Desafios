e828542488ba5dcfcb9ff8f70629ecda7c23fe4a
NutellaPy <91993233+NutellaPy@users.noreply.github.com>
2021-10-05 20:22:41 -0300

    Update and rename Desafio 002 to Desafio 002.py

--- mundo1/Desafio 002.py
+++ mundo1/Desafio 002.py
@@ -1,4 +1,4 @@
-Crie um script que armazene um nome e de um frase de boas vindas 
+#Crie um script que armazene um nome e de um frase de boas vindas 
 ===
 n=input('Digite seu nome: ')  
 print(f'Seja Bem-Vindo, \033[33m{n}\033[33m!') 
