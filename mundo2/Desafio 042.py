6dea390774ca51a278e77fb3b9b3966d71fb036d
NutellaPy <91993233+NutellaPy@users.noreply.github.com>
2021-10-06 12:12:55 -0300

    Create Desafio 042.py

---
+++ mundo2/Desafio 042.py
@@ -0,0 +1,16 @@
+print(17*'\033[35m-=\033[m')
+print('ANALISADOR AVANÇADO DE TRIANGULOS')
+print(17*'\033[35m-=\033[m')
+s1 = float(input('Digite o primeiro segmento: '))
+s2 = float(input('Digite o segundo segmento: '))
+s3 = float(input('Digite o terceiro segmento: '))
+if s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2: 
+  print('È possivel formar um triangulo') 
+  if s1 == s2 and s2 == s3:
+    print('O triangulo é EQUILÁTERO')
+  elif s1 == s2 and s2 != s3:
+    print('O triangulo é ISÓSCELES')
+  elif s1 != s2 and s2 != s3:
+    print('O triangulo é ESCALENO')
+else: 
+  print('nao é possivel formar um triangulo') 
