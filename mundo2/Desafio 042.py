3abd7a344be59a8acd0c3c76ed25cc8675da8561
NutellaPy <91993233+NutellaPy@users.noreply.github.com>
2021-10-06 12:26:22 -0300

    Update Desafio 042.py

--- mundo2/Desafio 042.py
+++ mundo2/Desafio 042.py
@@ -5,12 +5,12 @@ s1 = float(input('Digite o primeiro segmento: '))
 s2 = float(input('Digite o segundo segmento: '))
 s3 = float(input('Digite o terceiro segmento: '))
 if s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2: 
-  print('È possivel formar um triangulo') 
+  print('È possivel formar um triangulo', end =' ') 
   if s1 == s2 and s2 == s3:
-    print('O triangulo é EQUILÁTERO')
+    print('EQUILÁTERO')
   elif s1 == s2 and s2 != s3:
-    print('O triangulo é ISÓSCELES')
+    print('ISÓSCELES')
   elif s1 != s2 and s2 != s3:
-    print('O triangulo é ESCALENO')
+    print('ESCALENO')
 else: 
   print('nao é possivel formar um triangulo') 
